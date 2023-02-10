9b6bf997720d6b0ec418d4c50004f78d5798b0b8
0x0Louis <louis@joepegs.com>
2023-02-10 21:45:21 +0100

    add simple vault

---
+++ src/interfaces/ISimpleVault.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.10;
+
+import {IERC20Upgradeable} from "openzeppelin-upgradeable/token/ERC20/IERC20Upgradeable.sol";
+import {ILBPair} from "joe-v2/interfaces/ILBPair.sol";
+
+import {IStrategy} from "./IStrategy.sol";
+import {IBaseVault} from "./IBaseVault.sol";
+
+interface ISimpleVault is IBaseVault {
+    error SimpleVault__AmountsOverflow();
+}
