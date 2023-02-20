de54abc4522984096ea476c67fa9aae7999c568f
0x0Louis <louis@joepegs.com>
2023-02-20 17:31:11 +0100

    new distribution library

---
+++ src/libraries/Distribution.sol
@@ -0,0 +1,150 @@
+// SPDX-License-indexentifier: MIT
+
+pragma solidity 0.8.10;
+
+import {console} from "forge-std/console.sol";
+
+/**
+ * @title The Distribution library
+ * @author Trader Joe
+ * @notice This library is used to compute the distribution of a given amount of tokens.
+ */
+library Distribution {
+    error Distribution__CompositionFactorTooHigh();
+    error Distribution__AmountTooHigh();
+    error Distribution__PriceTooLow();
+
+    uint256 internal constant PRECISION = 1e18;
+
+    uint256 internal constant OFFSET = 128;
+    uint256 internal constant ONE = 1 << OFFSET;
+
+    /**
+     * @notice Returns the distribution following a given composition factor, price and amounts valued in Y.
+     * @param amountsInY The amounts of tokens to distribute.
+     * @param compositionFactor The composition factor.
+     * @param price The price of the token.
+     * @param index The index of the active amount.
+     * @return amountX The amount of token X.
+     * @return amountY The amount of token Y.
+     * @return distributionX The distribution of token X.
+     * @return distributionY The distribution of token Y.
+     */
+    function getDistributions(uint256[] memory amountsInY, uint256 compositionFactor, uint256 price, uint256 index)
+        internal
+        pure
+        returns (uint256 amountX, uint256 amountY, uint256[] memory distributionX, uint256[] memory distributionY)
+    {
+        if (compositionFactor > ONE) revert Distribution__CompositionFactorTooHigh();
+        if (price == 0) revert Distribution__PriceTooLow();
+
+        distributionX = new uint256[](amountsInY.length);
+        distributionY = new uint256[](amountsInY.length);
+
+        uint256 activeAmount = amountsInY[index];
+
+        if (activeAmount > type(uint128).max) revert Distribution__AmountTooHigh();
+
+        amountY = (activeAmount * compositionFactor) >> OFFSET;
+        amountX = ((activeAmount - amountY) << OFFSET) / price;
+
+        distributionX[index] = amountX;
+        distributionY[index] = amountY;
+
+        amountX = computeDistributionX(amountsInY, distributionX, price, amountX, index, true);
+        amountY = computeDistributionY(amountsInY, distributionY, amountY, index, true);
+    }
+
+    /**
+     * @notice Computes the distribution Y following a given composition factor, price and amounts valued in Y.
+     * @param amountsInY The amounts of tokens to distribute.
+     * @param distributionY The distribution of token Y.
+     * @param amountY The amount of token Y.
+     * @param end The index of the last amount to compute.
+     * @param endIsActive Whether the last amount is the active id.
+     * @return The amount of token Y.
+     */
+    function computeDistributionY(
+        uint256[] memory amountsInY,
+        uint256[] memory distributionY,
+        uint256 amountY,
+        uint256 end,
+        bool endIsActive
+    ) internal pure returns (uint256) {
+        for (uint256 i; i < end;) {
+            uint256 amountInY = amountsInY[i];
+            if (amountInY > type(uint128).max) revert Distribution__AmountTooHigh();
+
+            amountY += amountInY;
+            distributionY[i] = amountInY;
+
+            unchecked {
+                ++i;
+            }
+        }
+
+        computeDistribution(distributionY, amountY, 0, endIsActive ? end + 1 : end);
+
+        return amountY;
+    }
+
+    /**
+     * @notice Computes the distribution X following a given composition factor, price and amounts valued in Y.
+     * @param amountsInY The amounts of tokens to distribute.
+     * @param distributionX The distribution of token X.
+     * @param price The price of the token.
+     * @param amountX The amount of token X.
+     * @param start The index of the first amount to compute.
+     * @param startIsActive Whether the first amount is the active id.
+     * @return The amount of token X.
+     */
+    function computeDistributionX(
+        uint256[] memory amountsInY,
+        uint256[] memory distributionX,
+        uint256 price,
+        uint256 amountX,
+        uint256 start,
+        bool startIsActive
+    ) internal pure returns (uint256) {
+        for (uint256 i = startIsActive ? start + 1 : start; i < amountsInY.length;) {
+            uint256 amountInY = amountsInY[i];
+            uint256 amountInX = (amountInY << OFFSET) / price;
+
+            if (amountInY > type(uint128).max || amountInX > type(uint128).max) revert Distribution__AmountTooHigh();
+
+            amountX += amountInX;
+            distributionX[i] = amountInX;
+
+            unchecked {
+                ++i;
+            }
+        }
+
+        computeDistribution(distributionX, amountX, start, amountsInY.length);
+
+        return amountX;
+    }
+
+    /**
+     * @notice Computes the distribution from a given amount of tokens.
+     * Returns the percentage of each amount in the total amount.
+     * @param amounts The amounts of tokens to distribute.
+     * @param totalAmont The total amount of tokens to distribute.
+     * @param start The index of the first amount to compute.
+     * @param end The index of the last amount to compute.
+     */
+    function computeDistribution(uint256[] memory amounts, uint256 totalAmont, uint256 start, uint256 end)
+        internal
+        pure
+    {
+        if (totalAmont > 0) {
+            for (uint256 i = start; i < end;) {
+                amounts[i] = amounts[i] * PRECISION / totalAmont;
+
+                unchecked {
+                    ++i;
+                }
+            }
+        }
+    }
+}
---
+++ test/Distribution.t.sol
@@ -0,0 +1,190 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.10;
+
+import "forge-std/Test.sol";
+
+import "../src/libraries/Distribution.sol";
+
+contract DistributionTest is Test {
+    using Distribution for uint256[];
+
+    function testFuzz_ComputeDistribution(uint256[] memory amounts) external {
+        uint256[] memory distribution = new uint256[](amounts.length);
+
+        uint256 sum;
+        for (uint256 i; i < amounts.length; ++i) {
+            vm.assume(amounts[i] < type(uint256).max - sum && amounts[i] < type(uint256).max / 1e18);
+            distribution[i] = amounts[i];
+            sum += amounts[i];
+        }
+
+        distribution.computeDistribution(sum, 0, amounts.length);
+
+        for (uint256 i; i < distribution.length; ++i) {
+            if (sum != 0) assertEq(distribution[i], amounts[i] * 1e18 / sum, "testFuzz_ComputeDistribution::1");
+            else assertEq(distribution[i], 0, "testFuzz_ComputeDistribution::2");
+        }
+    }
+
+    function testFuzz_ComputeDistributionX(uint128[] memory amounts, uint256 price) external {
+        vm.assume(price > 0);
+
+        uint256[] memory amountsInY;
+
+        assembly {
+            amountsInY := amounts
+        }
+
+        uint256[] memory expectedDistributionX = new uint256[](amountsInY.length);
+
+        uint256 sum;
+        for (uint256 i; i < amountsInY.length; ++i) {
+            vm.assume(amountsInY[i] <= type(uint128).max && (amountsInY[i] << 128) / price <= type(uint128).max);
+
+            expectedDistributionX[i] = (amountsInY[i] << 128) / price;
+            sum += expectedDistributionX[i];
+        }
+
+        uint256[] memory distributionX = new uint256[](amountsInY.length);
+
+        amountsInY.computeDistributionX(distributionX, price, 0, 0, false);
+
+        for (uint256 i; i < amountsInY.length; ++i) {
+            if (sum != 0) {
+                assertEq(distributionX[i], expectedDistributionX[i] * 1e18 / sum, "test_ComputeDistributionX::1");
+            } else {
+                assertEq(distributionX[i], 0, "test_ComputeDistributionX::2");
+            }
+        }
+    }
+
+    function testFuzz_ComputeDistributionY(uint128[] memory amounts) external {
+        uint256[] memory amountsInY;
+
+        assembly {
+            amountsInY := amounts
+        }
+
+        uint256[] memory expectedDistributionY = new uint256[](amountsInY.length);
+
+        uint256 sum;
+        for (uint256 i; i < amountsInY.length; ++i) {
+            vm.assume(amountsInY[i] <= type(uint128).max);
+
+            expectedDistributionY[i] = amountsInY[i];
+            sum += expectedDistributionY[i];
+        }
+
+        uint256[] memory distributionY = new uint256[](amountsInY.length);
+
+        amountsInY.computeDistributionY(distributionY, 0, distributionY.length, false);
+
+        for (uint256 i; i < amountsInY.length; ++i) {
+            if (sum != 0) {
+                assertEq(distributionY[i], expectedDistributionY[i] * 1e18 / sum, "test_ComputeDistributionY::1");
+            } else {
+                assertEq(distributionY[i], 0, "test_ComputeDistributionY::2");
+            }
+        }
+    }
+
+    function test_GetDistributions() external {
+        uint256[] memory amounts = new uint256[](3);
+        (amounts[0], amounts[1], amounts[2]) = (1000e6, 1000e6, 1000e6);
+
+        uint256 price = (uint256(1000e6) << 128) / 1e18;
+        uint256 compositionFactor = (uint256(0.5e18) << 128) / 1e18;
+
+        (uint256 amountX, uint256 amountY, uint256[] memory distributionX, uint256[] memory distributionY) =
+            amounts.getDistributions(compositionFactor, price, 1);
+
+        assertEq(amountX, 1.5e18, "test_GetDistributions::1");
+        assertEq(amountY, 1500e6, "test_GetDistributions::2");
+
+        uint256 oneThird = uint256(1e18) / 3;
+
+        assertEq(distributionX[0], 0, "test_GetDistributions::3");
+        assertEq(distributionX[1], oneThird, "test_GetDistributions::4");
+        assertEq(distributionX[2], oneThird * 2, "test_GetDistributions::5");
+
+        assertEq(distributionY[0], oneThird * 2, "test_GetDistributions::6");
+        assertEq(distributionY[1], oneThird, "test_GetDistributions::7");
+        assertEq(distributionY[2], 0, "test_GetDistributions::8");
+    }
+
+    function testFuzz_GetDistributions(
+        uint128 compositionFactor,
+        uint128[] memory amounts,
+        uint256 price,
+        uint256 index
+    ) external {
+        vm.assume(compositionFactor <= 1 << 128 && index < amounts.length && price > 0);
+
+        uint256[] memory amountsInY;
+
+        assembly {
+            amountsInY := amounts
+        }
+
+        for (uint256 i = 0; i < amountsInY.length; i++) {
+            vm.assume((amountsInY[i] << 128) / price <= type(uint128).max);
+        }
+
+        (uint256 amountX, uint256 amountY, uint256[] memory distributionX, uint256[] memory distributionY) =
+            amountsInY.getDistributions(compositionFactor, price, index);
+
+        {
+            (uint256 sumDX, uint256 sumDY) = (0, 0);
+            for (uint256 i = 0; i < amountsInY.length; i++) {
+                uint256 x = amountX * distributionX[i] / 1e18;
+                uint256 y = amountY * distributionY[i] / 1e18;
+
+                sumDX += distributionX[i];
+                sumDY += distributionY[i];
+
+                assertLe((x * price >> 128) + y, amountsInY[i], "testFuzz_GetDistributions::1");
+            }
+
+            assertLe(sumDX, 1e18, "testFuzz_GetDistributions::2");
+            assertLe(sumDY, 1e18, "testFuzz_GetDistributions::3");
+        }
+
+        uint256[] memory expectedDX = new uint256[](amountsInY.length);
+        uint256[] memory expectedDY = new uint256[](amountsInY.length);
+
+        uint256 activeAmount = amountsInY[index];
+
+        uint256 sumY = (activeAmount * compositionFactor) >> 128;
+        uint256 sumX = ((activeAmount - sumY) << 128) / price;
+
+        expectedDY[index] = sumY;
+        expectedDX[index] = sumX;
+
+        for (uint256 i = 0; i < amountsInY.length; i++) {
+            if (i < index) {
+                expectedDY[i] = amountsInY[i];
+                sumY += expectedDY[i];
+            } else if (i > index) {
+                expectedDX[i] = (uint256(amountsInY[i]) << 128) / price;
+                sumX += expectedDX[i];
+            }
+        }
+
+        for (uint256 i = 0; i < amountsInY.length; i++) {
+            if (sumX > 0) expectedDX[i] = expectedDX[i] * 1e18 / sumX;
+            if (sumY > 0) expectedDY[i] = expectedDY[i] * 1e18 / sumY;
+
+            assertEq(
+                distributionX[i],
+                expectedDX[i],
+                string(abi.encodePacked("testFuzz_GetDistributions::4-", vm.toString(i)))
+            );
+            assertEq(
+                distributionY[i],
+                expectedDY[i],
+                string(abi.encodePacked("testFuzz_GetDistributions::5-", vm.toString(i)))
+            );
+        }
+    }
+}
